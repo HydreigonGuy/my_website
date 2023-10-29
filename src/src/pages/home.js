e1906c42d259fa3003333fb753ebfe0079cdcc14
HydreigonGuy <ad.thibault18@gmail.com>
2023-10-29 08:57:03 +0100

    updated website layout

--- src/src/pages/home.js
+++ src/src/pages/home.js
@@ -3,10 +3,12 @@ import picture_of_me from '../assets/images/me.png';
 function Home() {
   return (
     <>
+      <div className='home_header'>
         <img src={picture_of_me} className="picture_of_me" alt="me" />
-        <p>
-          My website!
-        </p>
+        <span>
+          Welcome to my website!
+        </span>
+      </div>
     </>
   );
 }
