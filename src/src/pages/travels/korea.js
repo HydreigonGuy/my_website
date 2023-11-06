55bc29df2d539d922f8aa01494e8b2ba4cbb0fe5
HydreigonGuy <ad.thibault18@gmail.com>
2023-11-06 14:53:36 +0100

    added cities to the korthe korea list

--- src/src/pages/travels/korea.js
+++ src/src/pages/travels/korea.js
@@ -1,11 +1,9 @@
 import picture_of_me from '../../assets/images/IMG20231014143340.jpg';
-import korea_picture_1 from '../../assets/images/IMG20230821101728.jpg';
-import korea_picture_2 from '../../assets/images/IMG20230821112915.jpg';
 import Busan_img from '../../assets/images/IMG20230821114142.jpg';
-import korea_picture_4 from '../../assets/images/IMG20230822162230.jpg';
-import korea_picture_5 from '../../assets/images/IMG20230822174058.jpg';
 import Daegu_img from '../../assets/images/IMG20230822174437.jpg';
 import Suwon_img from '../../assets/images/IMG20231104124118.jpg';
+import Sejong_img from '../../assets/images/IMG20231013141325.jpg';
+import Jochiwon_img from '../../assets/images/IMG20230926185028.jpg';
 
 import '../../assets/css/korean.css';
 import '../../assets/css/travels.css';
@@ -21,7 +19,9 @@ function Korea() {
         <br/>
         <TravelCityUrl name="Suwon" img={Suwon_img} />
         <TravelCityUrl name="Daegu" img={Daegu_img} />
+        <TravelCityUrl name="Sejong" img={Sejong_img} />
         <TravelCityUrl name="Busan" img={Busan_img} />
+        <TravelCityUrl name="Jochiwon" img={Jochiwon_img} />
       </div>
     </>
   );
