01f90c5962e16bb8d79c06a0d43d1dcbd0edd6c6
HydreigonGuy <ad.thibault18@gmail.com>
2023-11-06 13:49:55 +0100

    organised contents in korea route

--- src/src/pages/travels/korea.js
+++ src/src/pages/travels/korea.js
@@ -1,23 +1,27 @@
-import picture_of_me from '../../assets/images/me.png';
+import picture_of_me from '../../assets/images/IMG20231014143340.jpg';
 import korea_picture_1 from '../../assets/images/IMG20230821101728.jpg';
 import korea_picture_2 from '../../assets/images/IMG20230821112915.jpg';
-import korea_picture_3 from '../../assets/images/IMG20230821114142.jpg';
+import Busan_img from '../../assets/images/IMG20230821114142.jpg';
 import korea_picture_4 from '../../assets/images/IMG20230822162230.jpg';
 import korea_picture_5 from '../../assets/images/IMG20230822174058.jpg';
-import korea_picture_6 from '../../assets/images/IMG20230822174437.jpg';
+import Daegu_img from '../../assets/images/IMG20230822174437.jpg';
+import Suwon_img from '../../assets/images/IMG20231104124118.jpg';
 
 import '../../assets/css/korean.css';
+import '../../assets/css/travels.css';
+
+import TravelCityUrl from '../../components/travels_city_url';
 
 function Korea() {
   return (
     <>
-      <div className='korean_page'>
-        <img src={korea_picture_3} className="picture" />
-        <img src={korea_picture_4} className="picture" />
-        <img src={korea_picture_5} className="picture" />
-        <img src={korea_picture_1} className="picture" />
-        <img src={korea_picture_2} className="picture" />
-        <img src={korea_picture_6} className="picture" />
+      <div className='korean_page korean'>
+        <img src={picture_of_me} className="picture" />
+        <span>Here you can find pictures and information about my travels accross Korea!</span>
+        <br/>
+        <TravelCityUrl name="Suwon" img={Suwon_img} />
+        <TravelCityUrl name="Daegu" img={Daegu_img} />
+        <TravelCityUrl name="Busan" img={Busan_img} />
       </div>
     </>
   );
