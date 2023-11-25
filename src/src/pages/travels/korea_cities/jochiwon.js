16082369404f9c492157254971c39aa7be1a9752
HydreigonGuy <ad.thibault18@gmail.com>
2023-11-25 16:38:56 +0100

    added image loading to all korean city routes

--- src/src/pages/travels/korea_cities/jochiwon.js
+++ src/src/pages/travels/korea_cities/jochiwon.js
@@ -8,32 +8,43 @@ import festival_stands1 from '../../../assets/images/korea/jochiwon/IMG202311071
 import festival_stands2 from '../../../assets/images/korea/jochiwon/IMG20231108183101.jpg';
 // import festival_stands3 from '../../../assets/images/korea/jochiwon/IMG20230913181928.jpg';
 
+import rainy_festival_photo_small from '../../../assets/images/korea/jochiwon/small/IMG20230913191245.jpg';
+import foggy_gobok_reservoire_photo_small from '../../../assets/images/korea/jochiwon/small/IMG20230926185028.jpg';
+import nigh_school_library_photo_small from '../../../assets/images/korea/jochiwon/small/IMG20230930193817.jpg';
+import nigh_gobok_reservoire_photo_small from '../../../assets/images/korea/jochiwon/small/IMG20231022181150.jpg';
+import school_mountain_lake1_small from '../../../assets/images/korea/jochiwon/small/IMG20230902120218.jpg';
+import festival_stands1_small from '../../../assets/images/korea/jochiwon/small/IMG20231107134614.jpg';
+import festival_stands2_small from '../../../assets/images/korea/jochiwon/small/IMG20231108183101.jpg';
+// import festival_stands3_small from '../../../assets/images/korea/jochiwon/small/IMG20230913181928.jpg';
+
 import '../../../assets/css/korean.css';
 import '../../../assets/css/travels.css';
+
+import ProgressiveImg from '../../../components/image_loading';
 
 function Jochiwon() {
     return (
         <div className='korean_city_page korean'>
             <p className='jochiwon_title'>Jochiwon - 조치원</p>
-            <img src={school_mountain_lake1} className='jochiwon_header_img'/>
+            <ProgressiveImg src={school_mountain_lake1} placeholderSrc={school_mountain_lake1_small} className='jochiwon_header_img'/>
             <p>
                 Jochiwon is where I go to school, at Korea University Sejong Campus!
             </p>
-            <img src={nigh_school_library_photo} className='half_image'/>
+            <ProgressiveImg src={nigh_school_library_photo} placeholderSrc={nigh_school_library_photo_small} className='half_image'/>
             <span className='image_comment'>This is the school library.</span>
             <br/>
             <span className='image_comment_left_half'>
                 The school hosted several Festivals, when they do this, food trucs come and there is a stage where the school clubs can perform, there is singing, playing music, and dancing.
             </span>
-            <img src={rainy_festival_photo} className='half_image'/>
-            <img src={festival_stands1} className='half_image'/>
-            <img src={festival_stands2} className='half_image'/>
+            <ProgressiveImg src={rainy_festival_photo} placeholderSrc={rainy_festival_photo_small} className='half_image'/>
+            <ProgressiveImg src={festival_stands1} placeholderSrc={festival_stands1_small} className='half_image'/>
+            <ProgressiveImg src={festival_stands2} placeholderSrc={festival_stands2_small} className='half_image'/>
             <p>
                 Jochiwon has a reservoire surrounded by mountains.
                 Hiking there is quite nice, and the reservoire is surrounded by cafés, so once you get there you have a place to take a break!
             </p>
-            <img src={foggy_gobok_reservoire_photo} className='big_image'/>
-            <img src={nigh_gobok_reservoire_photo} className='big_image'/>
+            <ProgressiveImg src={foggy_gobok_reservoire_photo} placeholderSrc={foggy_gobok_reservoire_photo_small} className='big_image'/>
+            <ProgressiveImg src={nigh_gobok_reservoire_photo} placeholderSrc={nigh_gobok_reservoire_photo_small} className='big_image'/>
         </div>
     );
 }
--- src/src/pages/travels/korea_cities/sejong.js
+++ src/src/pages/travels/korea_cities/sejong.js
@@ -4,17 +4,24 @@ import plant_view_of_traditional_buildings from '../../../assets/images/korea/se
 import waterfall from '../../../assets/images/korea/sejong/IMG20230915143919.jpg';
 import arboretum_temple from '../../../assets/images/korea/sejong/IMG20231013141325.jpg';
 
+import header_photo_small from '../../../assets/images/korea/sejong/small/IMG20230915110832.jpg';
+import plant_view_of_traditional_buildings_small from '../../../assets/images/korea/sejong/small/IMG20230915110735.jpg';
+import waterfall_small from '../../../assets/images/korea/sejong/small/IMG20230915143919.jpg';
+import arboretum_temple_small from '../../../assets/images/korea/sejong/small/IMG20231013141325.jpg';
+
 import '../../../assets/css/korean.css';
 import '../../../assets/css/travels.css';
+
+import ProgressiveImg from '../../../components/image_loading';
 
 function Sejong() {
     return (
         <div className='korean_city_page korean'>
             <p className='sejong_title'>Sejong - 세종</p>
-            <img src={header_photo} className='sejong_header_img'/>
-            <img src={plant_view_of_traditional_buildings} className='big_image'/>
-            <img src={waterfall} className='big_image'/>
-            <img src={arboretum_temple} className='big_image'/>
+            <ProgressiveImg src={header_photo} placeholderSrc={header_photo_small} className='sejong_header_img'/>
+            <ProgressiveImg src={plant_view_of_traditional_buildings} placeholderSrc={plant_view_of_traditional_buildings_small} className='big_image'/>
+            <ProgressiveImg src={waterfall} placeholderSrc={waterfall_small} className='big_image'/>
+            <ProgressiveImg src={arboretum_temple} placeholderSrc={arboretum_temple_small} className='big_image'/>
         </div>
     );
 }
