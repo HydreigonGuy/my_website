d67fa2b3b0ed29bbbfb93763b0880e6555e45b72
HydreigonGuy <ad.thibault18@gmail.com>
2023-12-15 06:32:43 +0100

    added css and modularized the code for korean cheat sheets

--- src/src/pages/cheat_sheets/korean/korean.js
+++ src/src/pages/cheat_sheets/korean/korean.js
@@ -1,14 +1,18 @@
 
+import '../../../assets/css/korean.css';
+
+import KoreanCategoryLink from "../../../components/korean_category_link";
+
 function Korean() {
   return (
     <>
-      <div>
-        <h1>My Korean Cheat Sheet</h1>
-        <a href="/korean/verbs">Learn verbs</a>
-        <br/>
-        <a href="/korean/dates">Learn dates</a>
-        <br/>
-        <a href="/korean/animals">Learn animals</a>
+      <div className='korean_cheatsheet_body'>
+        <h1 className='korean_cheatsheet_title'>My Korean Cheat Sheet</h1>
+        <KoreanCategoryLink items={[
+          {"url":"/korean/verbs", "title":"Learn verbs"},
+          {"url":"/korean/dates", "title":"Learn dates"},
+          {"url":"/korean/animals", "title":"Learn animals"}
+        ]}/>
       </div>
     </>
   );
