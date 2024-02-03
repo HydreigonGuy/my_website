1a29dee781f04d66fef855a557f566d278742be0
HydreigonGuy <ad.thibault18@gmail.com>
2024-02-03 16:05:31 +0100

    updated korean verbs list

--- src/src/pages/cheat_sheets/korean/verbs.js
+++ src/src/pages/cheat_sheets/korean/verbs.js
@@ -31,6 +31,37 @@ function korean_verbs() {
               {"english":"To send", "korean":"보내다"},
               {"english":"To give", "korean":"주다"},
               {"english":"To recieve", "korean":"받다"},
+              {"english":"To sit", "korean":"앉다"},
+              {"english":"To know", "korean":"알다"},
+              {"english":"To chose", "korean":"고르다"},
+              {"english":"To raise", "korean":"기르다"},
+              {"english":"To want", "korean":"원하다"},
+              {"english":"To not know", "korean":"모르다"},
+              {"english":"To disappear", "korean":"사라지다"},
+              {"english":"Le leave / get out", "korean":"내리다"},
+              {"english":"To feed", "korean":"먹이다"},
+              {"english":"To be surprised", "korean":"놀랍다"},
+              {"english":"To help", "korean":"돕다"},
+              {"english":"To build", "korean":"짓다"},
+              {"english":"To draw", "korean":"긋다"},
+              {"english":"To connect", "korean":"잇다"},
+              {"english":"To recover", "korean":"낫다"},
+              {"english":"To find", "korean":"찾다"},
+              {"english":"To hide", "korean":"숨다"},
+              {"english":"To use", "korean":"사용하다"},
+              {"english":"To go / frequent", "korean":"다니다"},
+              {"english":"To get up / wake up", "korean":"일어나다"},
+              {"english":"To close", "korean":"닫다"},
+              {"english":"To open", "korean":"열다"},
+              {"english":"To clean", "korean":"청소하다"},
+              {"english":"To wipe", "korean":"닦다"},
+              {"english":"To fall asleep", "korean":"잠들다"},
+              {"english":"To play", "korean":"놀다"},
+              {"english":"To give", "korean":"드리다"},
+              {"english":"To wear", "korean":"입다"},
+              {"english":"To wear (on your feet)", "korean":"신다"},
+              {"english":"To take off (clothes)", "korean":"벗다"},
+              {"english":"To be born", "korean":"태어나다"},
             ]}/>
             <h3 className='korean_cheatsheet_subtitle'>Mooving</h3>
             <KoreanWord items={[
@@ -39,6 +70,7 @@ function korean_verbs() {
               {"english":"To drive", "korean":"운전하다"},
               {"english":"To fly", "korean":"날다"},
               {"english":"To visit", "korean":"방문하다"},
+              {"english":"To get out / leave", "korean":"나가다"},
             ]}/>
             <h3 className='korean_cheatsheet_subtitle'>Comunication</h3>
             <KoreanWord items={[
@@ -46,6 +78,8 @@ function korean_verbs() {
               {"english":"To read", "korean":"읽다"},
               {"english":"To call / sing", "korean":"부르다"},
               {"english":"To say / tell / talk", "korean":"말하다"},
+              {"english":"To listen", "korean":"듣다"},
+              {"english":"To ask", "korean":"묻다"},
             ]}/>
             <h3 className='korean_cheatsheet_subtitle'>Work</h3>
             <KoreanWord items={[
@@ -67,6 +101,15 @@ function korean_verbs() {
               {"english":"To be cheap", "korean":"싸다"},
               {"english":"To sell", "korean":"팔다"},
               {"english":"To pay", "korean":"내다"}
+            ]}/>
+            <h3 className='korean_cheatsheet_subtitle'>About someone</h3>
+            <KoreanWord items={[
+              {"english":"To enjoy", "korean":"즐기다"},
+              {"english":"To be thirsty", "korean":"목이 마르다"},
+              {"english":"To be sad", "korean":"슬프다"},
+              {"english":"To be healthy", "korean":"건강하다"},
+              {"english":"To be bored", "korean":"심심하다"},
+              {"english":"To be tired", "korean":"피곤하다"},
             ]}/>
             <h3 className='korean_cheatsheet_subtitle'>Adjective Verbs</h3>
             <KoreanWord items={[
@@ -91,6 +134,12 @@ function korean_verbs() {
               {"english":"To be in pain / to hurt", "korean":"아프다"},
               {"english":"To be low", "korean":"낮다"},
               {"english":"To be narrow / small", "korean":"좁다"},
+              {"english":"To be dry", "korean":"마르다"},
+              {"english":"To be different", "korean":"다르다"},
+              {"english":"To be good", "korean":"좋다"},
+              {"english":"To be boring", "korean":"지루하다"},
+              {"english":"To be dark", "korean":"어둡다"},
+              {"english":"To be fast", "korean":"빠르다"},
             ]}/>
         </div>
     );
