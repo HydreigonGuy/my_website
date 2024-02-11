34f015a3a3b1cf465bdcee1fc224fedb441b686c
HydreigonGuy <ad.thibault18@gmail.com>
2024-02-11 13:06:06 +0100

    started changes to korean part of website

--- src/src/pages/cheat_sheets/korean/korean.js
+++ src/src/pages/cheat_sheets/korean/korean.js
@@ -8,17 +8,26 @@ function Korean() {
     <>
       <div className='korean_cheatsheet_body'>
         <h1 className='korean_cheatsheet_title'>My Korean Cheat Sheet</h1>
-        <KoreanCategoryLink items={[
-          {"url":"/korean/verbs", "title":"Learn verbs"},
-          {"url":"/korean/dates", "title":"Learn dates"},
-          {"url":"/korean/food", "title":"Learn food"},
-          {"url":"/korean/school", "title":"Learn school vocabulary"},
-          {"url":"/korean/animals", "title":"Learn animals"},
-          {"url":"/korean/questions", "title":"Learn questions"},
-          {"url":"/korean/places", "title":"Learn places"},
-          {"url":"/korean/adjectives", "title":"Learn to describe"},
-          {"url":"/korean/vocabulary", "title":"Learn various words"},
-        ]}/>
+        <div className='half_screen'>
+          <h3 className='korean_cheatsheet_subtitle'>Vocabulary</h3>
+          <KoreanCategoryLink items={[
+            {"url":"/korean/verbs", "title":"List of verbs"},
+            {"url":"/korean/food", "title":"Food"},
+            {"url":"/korean/school", "title":"School vocabulary"},
+            {"url":"/korean/animals", "title":"Animals"},
+            {"url":"/korean/places", "title":"Learn places"},
+            {"url":"/korean/vocabulary", "title":"Learn various words"},
+          ]}/>
+        </div>
+        <div className='half_screen'>
+          <h3 className='korean_cheatsheet_subtitle'>Grammar</h3>
+          <KoreanCategoryLink items={[
+            {"url":"/korean/dates", "title":"Time"},
+            {"url":"/korean/places", "title":"Places"},
+            {"url":"/korean/questions", "title":"Questions"},
+            {"url":"/korean/adjectives", "title":"Adjectives"},
+          ]}/>
+        </div>
       </div>
     </>
   );
