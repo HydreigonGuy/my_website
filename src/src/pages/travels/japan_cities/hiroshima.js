9dbeec2ae9d6a26043f10c13fb1fb701adb97748
HydreigonGuy <ad.thibault18@gmail.com>
2024-03-18 05:18:51 +0100

    added imports for images in more japan cities

---
+++ src/src/pages/travels/japan_cities/hiroshima.js
@@ -0,0 +1,7 @@
+
+import city_sign_img_h from '../../../assets/images/japan/hiroshima/IMG20240225134208.jpg';
+import hiroshima_castle_img_v from '../../../assets/images/japan/hiroshima/IMG20240225141058.jpg';
+import a_bomb_dome_1_img_h from '../../../assets/images/japan/hiroshima/IMG20240225143641.jpg';
+import a_bomb_dome_2_img_h from '../../../assets/images/japan/hiroshima/IMG20240225143833.jpg';
+import a_bomb_dome_3_img_v from '../../../assets/images/japan/hiroshima/IMG20240225143953.jpg';
+import a_bomb_dome_4_img_h from '../../../assets/images/japan/hiroshima/IMG20240225144657.jpg';
