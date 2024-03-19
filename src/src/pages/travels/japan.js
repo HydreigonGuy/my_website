db8465f4b462d7abf6323761e806d2dcd3a47851
HydreigonGuy <ad.thibault18@gmail.com>
2024-03-19 07:27:34 +0100

    added comtent to tokyo page

--- src/src/pages/travels/japan.js
+++ src/src/pages/travels/japan.js
@@ -1,5 +1,6 @@
 // import img from '../../assets/images/japan/_.jpg';
 
+import '../../assets/css/japan.css';
 import '../../assets/css/travels.css';
 
 import TravelCityUrl from '../../components/travels_city_url';
@@ -9,11 +10,11 @@ function Japan() {
   return (
     <>
       <h1>Japan</h1>
-      <a href='/japan/tokyo'>Tokyo</a>
-      <a href='/japan/yokohama'>Yokohama</a>
-      <a href='/japan/kyoto'>Kyoto</a>
-      <a href='/japan/osaka'>Osaka</a>
-      <a href='/japan/hiroshima'>Hiroshima</a>
+      <a className='japan_city_url' href='/japan/tokyo'>Tokyo</a>
+      <a className='japan_city_url' href='/japan/yokohama'>Yokohama</a>
+      <a className='japan_city_url' href='/japan/kyoto'>Kyoto</a>
+      <a className='japan_city_url' href='/japan/osaka'>Osaka</a>
+      <a className='japan_city_url' href='/japan/hiroshima'>Hiroshima</a>
     </>
   );
 }
