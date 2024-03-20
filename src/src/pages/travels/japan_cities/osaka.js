386645ebcd9305441d77b0b2246626c598b4b46a
HydreigonGuy <ad.thibault18@gmail.com>
2024-03-20 15:45:59 +0100

    added contents to osaka page

--- src/src/pages/travels/japan_cities/osaka.js
+++ src/src/pages/travels/japan_cities/osaka.js
@@ -4,7 +4,7 @@ import universal_studio_2_harry_potter_enterance_img_v from '../../../assets/ima
 import universal_studio_3_hogwarts_express_img_v from '../../../assets/images/japan/osaka/IMG20240224093049.jpg';
 import universal_studio_4_harry_poter_castle_img_h from '../../../assets/images/japan/osaka/IMG20240224093342.jpg';
 import universal_studio_5_harry_poter_castle_img_v from '../../../assets/images/japan/osaka/IMG20240224093437.jpg';
-import universal_studio_6_harry_potter_living_car_img from '../../../assets/images/japan/osaka/IMG20240224093824.jpg';
+import universal_studio_6_harry_potter_living_car_img_v from '../../../assets/images/japan/osaka/IMG20240224093824.jpg';
 import universal_studio_7_pokemon_car_img_h from '../../../assets/images/japan/osaka/IMG20240224141722.jpg';
 import universal_studio_8_globe_img_v from '../../../assets/images/japan/osaka/IMG20240224142719.jpg';
 import universal_studio_9_gate_img_v from '../../../assets/images/japan/osaka/IMG20240224142843.jpg';
@@ -19,9 +19,38 @@ import osaka_castle_1_img_v from '../../../assets/images/japan/osaka/IMG20240225
 import osaka_castle_2_img_v from '../../../assets/images/japan/osaka/IMG20240225102650.jpg';
 import osaka_castle_plum_garden_img_h from '../../../assets/images/japan/osaka/IMG20240225103840.jpg';
 
+import '../../../assets/css/japan.css';
+import '../../../assets/css/travels.css';
+
 function Osaka() {
     return (
-            <p>Osaka</p>
+        <>
+            <img src={river_line_2_img_v} className="half_image" />
+            <img src={street_night_img_v} className="half_image" />
+
+            <img src={street_1_img_v} className="fourth_image" />
+            <img src={street_2_img_v} className="fourth_image" />
+            <img src={street_3_img_v} className="fourth_image" />
+            <img src={big_donkijote_img_v} className="fourth_image" />
+
+            <img src={osaka_castle_plum_garden_img_h} className="half_image" />
+            <img src={river_line_1_img_h} className="half_image" />
+    
+            <img src={osaka_castle_1_img_v} className="half_image" />
+            <img src={osaka_castle_2_img_v} className="half_image" />
+
+        {/* Universal Studio */}
+            <img src={universal_studio_8_globe_img_v} className="half_image" />
+            <img src={universal_studio_9_gate_img_v} className="half_image" />
+            <img src={universal_studio_1_minions_painting_img_v} className="half_image" />
+            <img src={universal_studio_6_harry_potter_living_car_img_v} className="half_image" />
+            <img src={universal_studio_7_pokemon_car_img_h} className="half_image" />
+            <img src={universal_studio_4_harry_poter_castle_img_h} className="half_image" />
+            <img src={universal_studio_2_harry_potter_enterance_img_v} className="half_image" />
+            <img src={universal_studio_3_hogwarts_express_img_v} className="half_image" />
+            <img src={universal_studio_5_harry_poter_castle_img_v} className="big_image" />
+
+        </>
     );
 }
 
