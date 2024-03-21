9b220c9b5d8de1fb8c2bf15ca6c8a87825b052a0
HydreigonGuy <ad.thibault18@gmail.com>
2024-03-21 08:01:00 +0100

    added pictures to japan travel page

--- src/src/pages/travels/japan.js
+++ src/src/pages/travels/japan.js
@@ -1,4 +1,9 @@
-// import img from '../../assets/images/japan/_.jpg';
+
+import tokyo_img from '../../assets/images/japan/tokyo/IMG20240217112418.jpg';
+import kyoto_img from '../../assets/images/japan/kyoto/IMG20240221172948.jpg';
+import osaka_img from '../../assets/images/japan/osaka/IMG20240224202110.jpg';
+import yokohama_img from '../../assets/images/japan/yokohama/IMG20240220164826.jpg';
+import hiroshima_img from '../../assets/images/japan/hiroshima/IMG20240225144657.jpg';
 
 import '../../assets/css/japan.css';
 import '../../assets/css/travels.css';
@@ -10,11 +15,26 @@ function Japan() {
   return (
     <>
       <h1>Japan</h1>
-      <a className='japan_city_url' href='/japan/tokyo'>Tokyo</a>
-      <a className='japan_city_url' href='/japan/yokohama'>Yokohama</a>
-      <a className='japan_city_url' href='/japan/kyoto'>Kyoto</a>
-      <a className='japan_city_url' href='/japan/osaka'>Osaka</a>
-      <a className='japan_city_url' href='/japan/hiroshima'>Hiroshima</a>
+      <a className='japan_city_url' href='/japan/tokyo'>
+            <img src={tokyo_img} />
+        Tokyo
+      </a>
+      <a className='japan_city_url' href='/japan/yokohama'>
+        <img src={yokohama_img} />
+        Yokohama
+      </a>
+      <a className='japan_city_url' href='/japan/kyoto'>
+            <img src={kyoto_img} />
+        Kyoto
+      </a>
+      <a className='japan_city_url' href='/japan/osaka'>
+        <img src={osaka_img} />
+        Osaka
+      </a>
+      <a className='japan_city_url' href='/japan/hiroshima'>
+        <img src={hiroshima_img} />
+        Hiroshima
+      </a>
     </>
   );
 }
