85604900cfc43fb5b648e3843169d2b1f9b08040
HydreigonGuy <ad.thibault18@gmail.com>
2024-05-16 09:06:59 +0200

    updated projects page, added css and language changing

--- src/src/components/project_info.js
+++ src/src/components/project_info.js
@@ -1,7 +1,14 @@
 
 function ProjectInfo(props) {
+  const text = {
+    "Go":{"eng":"Go", "fr":"Go"},
+    "View the code":{"eng":"View the code", "fr":"Voire le code"},
+  } 
   return (
     <div className="project-content">
+        {props.image &&
+            <img src={props.image} className='project-content-image' />
+        }
         <h3>{props.title}</h3>
         <p>
             {props.info}
@@ -10,13 +17,13 @@ function ProjectInfo(props) {
         <div className="links-area">
             {props.try_url && props.code_url ?
                 <>
-                    <a href={props.try_url} className="half-url-content" target="_blank">Go</a>
-                    <a href={props.code_url} className="half-url-content" target="_blank">View the code</a>
+                    <a  href={props.try_url} className="half-url-content project-content-url" target="_blank">{text["Go"][props.lang]}</a>
+                    <a href={props.code_url} className="half-url-content project-content-url" target="_blank">{text["View the code"][props.lang]}</a>
                 </>
             : props.try_url ?
-                <a href={props.try_url} target="_blank">Go</a>
+                <a className="project-content-url" href={props.try_url} target="_blank">{text["Go"][props.lang]}</a>
             : props.code_url &&
-                <a href={props.code_url} target="_blank">View the code</a>
+                <a className="project-content-url" href={props.code_url} target="_blank">{text["View the code"][props.lang]}</a>
             }
         </div>
     </div>
--- src/src/pages/pro/projects.js
+++ src/src/pages/pro/projects.js
@@ -1,40 +1,73 @@
 
 import '../../assets/css/projects.css';
 
+import autone_img from '../../assets/images/pro/autone.png';
+import my_web_img from '../../assets/images/pro/my_web.png';
+import my_rpg_img from '../../assets/images/pro/my_rpg.png';
+
 import ProjectInfo from '../../components/project_info';
 
+import { useSearchParams } from "react-router-dom";
+
 function Projects() {
+    const text = {
+        "Projects":{"eng":"Projects", "fr":"Projets"},
+        "My Website":{"eng":"My Website", "fr":"Mon Site Web"},
+        "":{"eng":"", "fr":""},
+    }
+    let [searchParams, setSearchParams] = useSearchParams();
+    var lang = searchParams.get("lang")
+
+    if (!["eng", "fr"].includes(lang))
+        lang = "eng"
     return (
         <>
+            <div className="projects-header">
+                <h1>{text["Projects"][lang]}</h1>
+
+                <div className='language-selector-container'>
+                    <a className='language-selector' href='?lang=eng'>ENG</a>
+                    <a className='language-selector' href='?lang=fr'>FR</a>
+                </div>
+            </div>
+            <div className="prijects-body">
             <ProjectInfo
                 title="Autone"
                 info="Autone is a website and mobile application that generates music sheets from a given music."
                 try_url="https://autone.app"
+                image={autone_img}
+                lang={lang}
                 />
 
             <ProjectInfo
-                title="My Website"
+                title={text["My Website"][lang]}
                 info="This is my website! A website made in REACT."
                 try_url="https://adrien-thibault.de"
                 code_url="https://github.com/HydreigonGuy/my_website"
+                image={my_web_img}
+                lang={lang}
                 />
 
             <ProjectInfo
                 title="My RPG"
                 info="I made an RPG in C with the CSFML library!"
                 code_url="https://github.com/HydreigonGuy/my_rpg"
+                image={my_rpg_img}
+                lang={lang}
                 />
             
             <ProjectInfo
                 title="My Malloc"
                 info="I recoded malloc in C."
                 code_url="https://github.com/HydreigonGuy/malloc"
+                lang={lang}
                 />
 
             <ProjectInfo
                 title="Tekspice"
                 info=""
                 code_url="https://github.com/HydreigonGuy/tekspice"
+                lang={lang}
                 />
             
             <ProjectInfo
@@ -44,19 +77,23 @@ function Projects() {
                 The AI moves around and tries to collect ressources to multiply and level up.
                 Multiple AIs can connect and the AI that reaches the highest level wins!"
                 code_url="https://github.com/HydreigonGuy/zappy"
+                lang={lang}
                 />
 
             <ProjectInfo
                 title="Glados"
                 info="This is a project where as a group we created our own language and made a compiler and an interpreter for it."
                 code_url="https://github.com/HydreigonGuy/glados"
+                lang={lang}
                 />
 
             <ProjectInfo
                 title="My Defender"
                 info="I made a tower defence game in C with the CSFML library!"
                 code_url="https://github.com/HydreigonGuy/my_defender"
+                lang={lang}
                 />
+            </div>
 
         </>
     );
