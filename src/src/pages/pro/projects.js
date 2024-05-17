0fb40e25c86e5e227eabba64cd59e06b823d727b
HydreigonGuy <ad.thibault18@gmail.com>
2024-05-17 16:42:05 +0200

    improved projects page visually

--- src/src/pages/pro/projects.js
+++ src/src/pages/pro/projects.js
@@ -31,89 +31,107 @@ function Projects() {
                 </div>
             </div>
             <div className="prijects-body">
-            <ProjectInfo
-                title="Autone"
-                info="Autone is a website and mobile application that generates music sheets from a given music."
-                try_url="https://autone.app"
-                image={autone_img}
-                lang={lang}
-                />
+            <h3 className='projects-label'>Top Projects</h3>
+            <div className='project-label-line'/>
+            <div className='project-list'>
+                <ProjectInfo
+                    title="Autone"
+                    info="Autone is a website and mobile application that generates music sheets from a given music."
+                    try_url="https://autone.app"
+                    image={autone_img}
+                    lang={lang}
+                    />
+            </div>
 
-            <ProjectInfo
-                title={text["My Website"][lang]}
-                info="This is my website! A website made in REACT."
-                try_url="https://adrien-thibault.de"
-                code_url="https://github.com/HydreigonGuy/my_website"
-                image={my_web_img}
-                lang={lang}
-                />
+            <div className='project-list'>
+                <ProjectInfo
+                    title={text["My Website"][lang]}
+                    info="This is my website! A website made in REACT."
+                    try_url="https://adrien-thibault.de"
+                    code_url="https://github.com/HydreigonGuy/my_website"
+                    image={my_web_img}
+                    lang={lang}
+                    />
+            </div>
 
-            <ProjectInfo
-                title="My RPG"
-                info="I made an RPG in C with the CSFML library!"
-                code_url="https://github.com/HydreigonGuy/my_rpg"
-                image={my_rpg_img}
-                lang={lang}
-                />
+            <div className='project-list'>
+                <ProjectInfo
+                    title="My RPG"
+                    info="I made an RPG in C with the CSFML library!"
+                    code_url="https://github.com/HydreigonGuy/my_rpg"
+                    image={my_rpg_img}
+                    lang={lang}
+                    />
+            </div>
+
+            <h3 className='projects-label'>School Projects</h3>
+            <div className='project-label-line'/>
             
-            <ProjectInfo
-                title="My Malloc"
-                info="I recoded malloc in C."
-                code_url="https://github.com/HydreigonGuy/malloc"
-                lang={lang}
-                />
+            <div className='project-list'>
+                <ProjectInfo
+                    title="Zappy"
+                    info="Zappy is a project in C of a game, not to be played by humans, but by an AI.
+                    There is a server which runs the game and a client that runs an AI that will play the game.
+                    The AI moves around and tries to collect ressources to multiply and level up.
+                    Multiple AIs can connect and the AI that reaches the highest level wins!"
+                    code_url="https://github.com/HydreigonGuy/zappy"
+                    lang={lang}
+                    />
 
-            <ProjectInfo
-                title="Tekspice"
-                info=""
-                code_url="https://github.com/HydreigonGuy/tekspice"
-                lang={lang}
-                />
-            
-            <ProjectInfo
-                title="Zappy"
-                info="Zappy is a project in C of a game, not to be played by humans, but by an AI.
-                There is a server which runs the game and a client that runs an AI that will play the game.
-                The AI moves around and tries to collect ressources to multiply and level up.
-                Multiple AIs can connect and the AI that reaches the highest level wins!"
-                code_url="https://github.com/HydreigonGuy/zappy"
-                lang={lang}
-                />
+                <ProjectInfo
+                    title="My Malloc"
+                    info="I recoded malloc in C."
+                    code_url="https://github.com/HydreigonGuy/malloc"
+                    lang={lang}
+                    />
+                
+                <ProjectInfo
+                    title="Tekspice"
+                    info=""
+                    code_url="https://github.com/HydreigonGuy/tekspice"
+                    lang={lang}
+                    />
+            </div>
 
-            <ProjectInfo
-                title="Glados"
-                info="This is a project where as a group we created our own language and made a compiler and an interpreter for it."
-                code_url="https://github.com/HydreigonGuy/glados"
-                lang={lang}
-                />
+            <div className='project-list'>
+                <ProjectInfo
+                    title="Glados"
+                    info="This is a project where as a group we created our own language and made a compiler and an interpreter for it."
+                    code_url="https://github.com/HydreigonGuy/glados"
+                    lang={lang}
+                    />
+                
+                <ProjectInfo
+                    title="Wolfram"
+                    info="Project in Haskell."
+                    code_url="https://github.com/HydreigonGuy/wolfram"
+                    lang={lang}
+                    />
 
-            <ProjectInfo
-                title="My Defender"
-                info="I made a tower defence game in C with the CSFML library!"
-                code_url="https://github.com/HydreigonGuy/my_defender"
-                lang={lang}
-                />
+                <ProjectInfo
+                    title="My Teams"
+                    info="Recoding a microsoft teams like messaging app in C."
+                    code_url="https://github.com/HydreigonGuy/my_teams"
+                    lang={lang}
+                    />
+            </div>
 
-            <ProjectInfo
+            <div className='project-list'>
+                <ProjectInfo
+                    title="My Defender"
+                    info="I made a tower defence game in C with the CSFML library!"
+                    code_url="https://github.com/HydreigonGuy/my_defender"
+                    lang={lang}
+                    />
+                
+                <ProjectInfo
                 title="My Hunter"
                 info="This is the first game that I coded, it was in C with the CSFML library, my first time trying this library. It is a simple hunter game where you need to click on enemies running accross your screen."
                 code_url="https://github.com/HydreigonGuy/my_hunter"
                 lang={lang}
                 />
+            </div>
 
-            <ProjectInfo
-                title="Wolfram"
-                info="Project in Haskell."
-                code_url="https://github.com/HydreigonGuy/wolfram"
-                lang={lang}
-                />
-
-            <ProjectInfo
-                title="My Teams"
-                info="Recoding a microsoft teams like messaging app in C."
-                code_url="https://github.com/HydreigonGuy/my_teams"
-                lang={lang}
-                />
             </div>
 
         </>
