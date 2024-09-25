156443c25e5bac85071efa5cdcee3643ab4e1898
HydreigonGuy <ad.thibault18@gmail.com>
2024-09-25 20:45:14 +0200

    added images and css to France page

--- src/src/pages/travels/france.js
+++ src/src/pages/travels/france.js
@@ -5,7 +5,17 @@ import '../../assets/css/france.css';
 function France() {
   return (
     <>
-      <h1 className=''>France</h1>
+      <h1 className='title'>France</h1>
+      <div className='images_body'>
+        <div className='cityTitleBlock'>
+          <h3 className='cityTitle'>Paris</h3>
+          <h3 className='cityTitle'>Toulouse</h3>
+          <h3 className='cityTitle'>Niort</h3>
+        </div>
+        <img className='image' src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/paris/IMG20240914202245.jpg"/>
+        <img className='image' src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20240711104504.jpg"/>
+        <img className='image' src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/niort/IMG20240809142103.jpg" />
+      </div>
     </>
   );
 }
