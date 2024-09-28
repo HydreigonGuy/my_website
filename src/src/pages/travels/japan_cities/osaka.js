f8cc4d20486488c96d9014c73d223606d015ca67
HydreigonGuy <ad.thibault18@gmail.com>
2024-09-29 00:11:23 +0200

    changed japan images hosting

--- src/src/pages/travels/japan_cities/osaka.js
+++ src/src/pages/travels/japan_cities/osaka.js
@@ -1,54 +1,34 @@
 
-import universal_studio_1_minions_painting_img_v from '../../../assets/images/japan/osaka/IMG20240224083356.jpg';
-import universal_studio_2_harry_potter_enterance_img_v from '../../../assets/images/japan/osaka/IMG20240224093032.jpg';
-import universal_studio_3_hogwarts_express_img_v from '../../../assets/images/japan/osaka/IMG20240224093049.jpg';
-import universal_studio_4_harry_poter_castle_img_h from '../../../assets/images/japan/osaka/IMG20240224093342.jpg';
-import universal_studio_5_harry_poter_castle_img_v from '../../../assets/images/japan/osaka/IMG20240224093437.jpg';
-import universal_studio_6_harry_potter_living_car_img_v from '../../../assets/images/japan/osaka/IMG20240224093824.jpg';
-import universal_studio_7_pokemon_car_img_h from '../../../assets/images/japan/osaka/IMG20240224141722.jpg';
-import universal_studio_8_globe_img_v from '../../../assets/images/japan/osaka/IMG20240224142719.jpg';
-import universal_studio_9_gate_img_v from '../../../assets/images/japan/osaka/IMG20240224142843.jpg';
-import street_1_img_v from '../../../assets/images/japan/osaka/IMG20240224162025.jpg';
-import street_2_img_v from '../../../assets/images/japan/osaka/IMG20240224162210.jpg';
-import street_3_img_v from '../../../assets/images/japan/osaka/IMG20240224162236.jpg';
-import big_donkijote_img_v from '../../../assets/images/japan/osaka/IMG20240224171828.jpg';
-import river_line_1_img_h from '../../../assets/images/japan/osaka/IMG20240224181013.jpg';
-import river_line_2_img_v from '../../../assets/images/japan/osaka/IMG20240224181440.jpg';
-import street_night_img_v from '../../../assets/images/japan/osaka/IMG20240224202110.jpg';
-import osaka_castle_1_img_v from '../../../assets/images/japan/osaka/IMG20240225102507.jpg';
-import osaka_castle_2_img_v from '../../../assets/images/japan/osaka/IMG20240225102650.jpg';
-import osaka_castle_plum_garden_img_h from '../../../assets/images/japan/osaka/IMG20240225103840.jpg';
-
 import '../../../assets/css/japan.css';
 import '../../../assets/css/travels.css';
 
 function Osaka() {
     return (
         <>
-            <img src={river_line_2_img_v} className="half_image" />
-            <img src={street_night_img_v} className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224181440.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224202110.jpg" className="half_image" />
 
-            <img src={street_1_img_v} className="fourth_image" />
-            <img src={street_2_img_v} className="fourth_image" />
-            <img src={street_3_img_v} className="fourth_image" />
-            <img src={big_donkijote_img_v} className="fourth_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224162025.jpg" className="fourth_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224162210.jpg" className="fourth_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224162236.jpg" className="fourth_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224171828.jpg" className="fourth_image" />
 
-            <img src={osaka_castle_plum_garden_img_h} className="half_image" />
-            <img src={river_line_1_img_h} className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240225103840.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224181013.jpg" className="half_image" />
     
-            <img src={osaka_castle_1_img_v} className="half_image" />
-            <img src={osaka_castle_2_img_v} className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240225102507.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240225102650.jpg" className="half_image" />
 
         {/* Universal Studio */}
-            <img src={universal_studio_8_globe_img_v} className="half_image" />
-            <img src={universal_studio_9_gate_img_v} className="half_image" />
-            <img src={universal_studio_1_minions_painting_img_v} className="half_image" />
-            <img src={universal_studio_6_harry_potter_living_car_img_v} className="half_image" />
-            <img src={universal_studio_7_pokemon_car_img_h} className="half_image" />
-            <img src={universal_studio_4_harry_poter_castle_img_h} className="half_image" />
-            <img src={universal_studio_2_harry_potter_enterance_img_v} className="half_image" />
-            <img src={universal_studio_3_hogwarts_express_img_v} className="half_image" />
-            <img src={universal_studio_5_harry_poter_castle_img_v} className="big_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224142719.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224142843.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224083356.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224093824.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224141722.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224093342.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224093032.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224093049.jpg" className="half_image" />
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/osaka/IMG20240224093437.jpg" className="big_image" />
 
         </>
     );
