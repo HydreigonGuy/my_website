dfdf6515b51363572fce9e81674530fa0cbbe89b
HydreigonGuy <ad.thibault18@gmail.com>
2024-10-30 22:10:46 +0100

    added remaining links to korea page

--- src/src/pages/travels/korea.js
+++ src/src/pages/travels/korea.js
@@ -15,12 +15,15 @@ function Korea() {
         <ProgressiveImg src={picture_of_me} placeholderSrc={picture_of_me_small} className="picture" />
         <span>Here you can find pictures and information about my travels accross Korea!</span>
         <br/>
-        <TravelCityUrl name="Suwon" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/suwon/IMG20231104124118.jpg" comment="수원" url="/korea/suwon" />
+        <TravelCityUrl name="Seoul" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/seoul/IMG20231014142928.jpg" comment="서울" url="/korea/seoul" />
+        <TravelCityUrl name="Jeju" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/jeju/IMG20240427100337.jpg" comment="제주" url='/korea/jeju' />
         <TravelCityUrl name="Daegu" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/daegu/IMG20230822174437.jpg" comment="대구" url='/korea/daegu' />
-        <TravelCityUrl name="Sejong" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/sejong/IMG20231013141325.jpg" comment="세종" url="/korea/sejong" />
         <TravelCityUrl name="Busan" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/busan/IMG20230821114142.jpg" comment="부산" url="/korea/busan" />
-        <TravelCityUrl name="Jochiwon" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/jochiwon/IMG20240410174732.jpg" comment="조치원" url="/korea/jochiwon" />
         <TravelCityUrl name="Gwangju" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/gwangju/IMG20231202122651.jpg" comment="광주" url="/korea/gwangju" />
+        <TravelCityUrl name="Daejeon" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/daejeon/IMG20231002194202.jpg" comment="대전" url="/korea/daejeon" />
+        <TravelCityUrl name="Suwon" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/suwon/IMG20231104124118.jpg" comment="수원" url="/korea/suwon" />
+        <TravelCityUrl name="Sejong" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/sejong/IMG20231013141325.jpg" comment="세종" url="/korea/sejong" />
+        <TravelCityUrl name="Jochiwon" img="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/jochiwon/IMG20240118152505.jpg" comment="조치원" url="/korea/jochiwon" />
       </div>
     </>
   );
