809a8ce56814776e87ddee8fae544b9f2ff37f4a
HydreigonGuy <ad.thibault18@gmail.com>
2025-01-18 22:59:22 +0100

    added pictures to Toulouse

--- src/src/pages/travels/france_cities/toulouse.js
+++ src/src/pages/travels/france_cities/toulouse.js
@@ -11,6 +11,14 @@ function Toulouse() {
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20240711105518.jpg" className='third_img'/>
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20240711110809.jpg" className='full_half_img'/>
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20240711105431.jpg" className='full_half_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241011162640.jpg" className='third_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241026210110.jpg" className='third_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241031221159.jpg" className='third_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241214142830.jpg" className='third_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241215103528.jpg" className='third_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241214145114.jpg" className='third_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241215104728.jpg" className='full_half_img'/>
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/toulouse/IMG20241011162356.jpg" className='full_half_img'/>
         </div>
     );
 }
