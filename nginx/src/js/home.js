38606eecac7f49b55a26132061f7ad7771d0c8a4
HydreigonGuy <ad.thibault18@gmail.com>
2025-04-26 22:27:19 +0200

    added images to travel page

--- nginx/src/js/home.js
+++ nginx/src/js/home.js
@@ -112,11 +112,26 @@ function getFooterContents() {
 
 function getTravelPage() {
     return '<h3 class="content_title">' + travel_page_map.title[lang] + '</h3>\
-        <a href="/france">' + travel_page_map.france[lang] + '</a>\
-        <a href="/korea">' + travel_page_map.korea[lang] + '</a>\
-        <a href="/china">' + travel_page_map.china[lang] + '</a>\
-        <a href="/japan">' + travel_page_map.japan[lang] + '</a>\
-        <a href="/uk">' + travel_page_map.uk[lang] + '</a>'
+        <a class="travel_url" href="/france">\
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/paris/IMG20240914202245.jpg"/>\
+            ' + travel_page_map.france[lang] + '\
+        </a>\
+        <a class="travel_url" href="/korea">\
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/seoul/IMG20231014142928.jpg" />\
+            ' + travel_page_map.korea[lang] + '\
+        </a>\
+        <a class="travel_url" href="/china">\
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/china/shang-hai/IMG20240624200828.jpg" />\
+            ' + travel_page_map.china[lang] + '\
+        </a>\
+        <a class="travel_url" href="/japan">\
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/kyoto/IMG20240222094915.jpg" />\
+            ' + travel_page_map.japan[lang] + '\
+        </a>\
+        <a class="travel_url" href="/uk">\
+            <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/uk/york/IMG20241113162256.jpg" />\
+            ' + travel_page_map.uk[lang] + '\
+        </a>'
 }
 
 function getCVPage() {
