00075b0f1c883e389ee389c6ff364394c54a539d
HydreigonGuy <ad.thibault18@gmail.com>
2025-05-01 17:43:20 +0200

    added nginx japan page and updated html of other country pages

--- nginx/src/js/home.js
+++ nginx/src/js/home.js
@@ -131,20 +131,16 @@ function getTravelPage() {
         <a class="travel_url" href="/france">\
             <p>' + travel_page_map.france[lang] + '</p>\
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/paris/IMG20240914202245.jpg"/>\
-        </a>\
-        <a class="travel_url" href="/korea">\
+        </a><a class="travel_url" href="/korea">\
             <p>' + travel_page_map.korea[lang] + '</p>\
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/seoul/IMG20231014142928.jpg" />\
-        </a>\
-        <a class="travel_url" href="/china">\
+        </a><a class="travel_url" href="/china">\
             <p>' + travel_page_map.china[lang] + '</p>\
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/china/shang-hai/IMG20240624200828.jpg" />\
-        </a>\
-        <a class="travel_url" href="/japan">\
+        </a><a class="travel_url" href="/japan">\
             <p>' + travel_page_map.japan[lang] + '</p>\
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/kyoto/IMG20240222094915.jpg" />\
-        </a>\
-        <a class="travel_url" href="/uk">\
+        </a><a class="travel_url" href="/uk">\
             <p>' + travel_page_map.uk[lang] + '</p>\
             <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/uk/york/IMG20241113162256.jpg" />\
         </a>'
