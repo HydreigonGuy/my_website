a556a058fa5846ac1b29668b188a08ff6ce88964
adrien <ad.thibault18@gmail.com>
2025-12-17 18:40:01 +0100

    added country links and  images to new travels page

--- src/js/travels/travels.js
+++ src/js/travels/travels.js
@@ -35,6 +35,24 @@ function formatTravelContents() {
         <div class="travel_page_lang_btn_container">
             <button>FR</button> | <button>ENG</button>
         </div>
+        <div class="travel_url_container">
+            <a class="travel_url" href="/france">
+                <p>${travel_page_map.france[lang]}</p>
+                <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/france/paris/IMG20240914202245.jpg"/>
+            </a><a class="travel_url" href="/korea">
+                <p>${travel_page_map.korea[lang]}</p>
+                <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/korea/seoul/IMG20231014142928.jpg" />
+            </a><a class="travel_url" href="/china">
+                <p>${travel_page_map.china[lang]}</p>
+                <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/china/shang-hai/IMG20240624200828.jpg" />
+            </a><a class="travel_url" href="/japan">
+                <p>${travel_page_map.japan[lang]}</p>
+                <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/japan/kyoto/IMG20240222094915.jpg" />
+            </a><a class="travel_url" href="/uk">
+                <p>${travel_page_map.uk[lang]}</p>
+                <img src="https://raw.githubusercontent.com/HydreigonGuy/my_website/refs/heads/images/images/travels/uk/york/IMG20241113162256.jpg" />
+            </a>
+        </div>
     </div>`
 }
 
